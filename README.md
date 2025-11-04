2ec4e8373b0c507c41fc76ed1dcc0d8e96bcd069
MichaelPH729 <michaelpaulhodges@gmail.com>
2025-11-04 01:33:09 -0500

    Refactored to clean up code and fix the list error- see readme for next steps

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 TO activate the virtual environment when editing:
 
 source .venv/bin/activate
+
+TODO - add the --verbose flag 
+consider refactoring the sys.argv[1:] call to argparse instead 
+https://docs.python.org/3/library/argparse.html
--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+prompt ="".join(sys.argv[1:])
 
 def main():
-    prompt = sys.argv[1:]
-    if len(prompt) < 1:
+    if not prompt:
+        print("AI Assistant requires a string input, EX: python main.py 'What are the most important words a man can say?'")
         sys.exit(1)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
